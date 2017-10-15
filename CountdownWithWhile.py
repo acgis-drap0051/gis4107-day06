8d464a1cc9982380168617b7d74483310291082c
MCE-SMM-ADVGIS\MCE <drap0051@algonquinlive.com>
2017-10-14 23:24:29 -0400

    Question

---
+++ CountdownWithWhile.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      MCE
+#
+# Created:     10/10/2017
+# Copyright:   (c) MCE 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+Countdown = 10
+while Countdown >= 0:
+    print Countdown
+    Countdown -= 1
+
+
+
